3293900f090d0e5ffd4add381d363035932facca
lfaf373 <lfaf373@gmail.com>
2016-09-30 08:30:17 -0400

    Final Assignment

--- Assignment7class.R
+++ Assignment7class.R
@@ -14,13 +14,15 @@ View(data_long)
 # Converting long format into seperate columns, seperating by type of measure
 instructor_data <- spread(data_long, variables, measure)
 # upload student data
-student_data<- read.csv("C:/Users/lfaf373/Documents/RStudioProj/cLASS7/student_activity.csv")
+data_wide2<- read.csv("C:/Users/lfaf373/Documents/RStudioProj/cLASS7/student_activity.csv")
+#data_long2 <- gather(data_wide2, measure, variable)
+student_data <- spread(data_wide2, variable, measure)
 # first we have to make sure r knows to use dplyr command
 # Here we are only including data from the second clas
 student_data_2 <- dplyr::filter(student_data, date == 20160204)
 # Here we are only including data from students who sat at table 4
 # Subsetted from the original dataset
-student_data_3 <- dplyr::filter(student_data, variable == "table" & measure == 4)
+student_data_3 <- dplyr::filter(student_data, table == 4)
 # usinging mutate to make a new variable, total sleep... which is lite plus deep sleep
 instructor_data <- dplyr::mutate(instructor_data, total_sleep = s_deep + s_light)
 #making a new data frame that only includes total_sleep 
@@ -33,8 +35,22 @@ student_data <- dplyr::mutate(student_data, week = dplyr::ntile(date, 3))
 #
 # motivation <- dplyr::filter(student_data, variable == "motivation")
 # Summarize data
-dplyr::summarise(student_data, avg=mean(variable))
+student_data %>% dplyr::summarise(mean(motivation))
 
 #That isn't super interesting, so let's break it down by week:
 
 student_data %>% dplyr::group_by(date) %>% dplyr::summarise(mean(motivation))
+
+#Create two new data sets using this method. One that sumarizes average motivation for students for each week (student_week) 
+# and another than sumarizes "m_active_time" for the instructor per week (instructor_week).
+
+student_week<-student_data %>% dplyr::group_by(week) %>% dplyr::summarise(mean(motivation))
+instructor_week<-instructor_data %>% dplyr::group_by(week) %>% dplyr::summarise(mean(m_active_time))
+
+#merge these two data set
+merge <- dplyr::full_join(instructor_week, student_week, "week")
+
+# plot mean motivation with mean activity time
+names(merge) <- c("week", "m2", "m3")
+plotting<-cor(merge$m2, merge$m3)
+plot(merge$m2, merge$m3)
